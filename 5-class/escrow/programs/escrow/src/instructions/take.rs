f784c8b9b23683cbf57585830ca05d79fb8f45ea
krkbheet@gmail.com <krkbheet@gmail.com>
2025-04-20 10:38:09 +0200

    C5 feat: add has one to check the escrow account token and authority

--- 5-class/escrow/programs/escrow/src/instructions/take.rs
+++ 5-class/escrow/programs/escrow/src/instructions/take.rs
@@ -56,6 +56,10 @@ pub struct Take<'info> {
 
     #[account(
         mut,
+        // check if the parameters are the same as on the escrow
+        has_one = mint_a,
+        has_one = mint_b,
+        has_one = maker,
         seeds = [b"escrow", escrow.maker.key().as_ref(), escrow.seed.to_le_bytes().as_ref()],
         bump = escrow.bump,
         close = maker
